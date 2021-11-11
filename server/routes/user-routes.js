df2fc020bb2cc5a83eec5a05c80cc1bf5fd1930a
Andrew Keiser <webdev410@gmail.com>
2021-11-11 12:40:23 -0700

    added post routes

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -24,5 +24,62 @@ router.get("/users", (req, res) => {
 		}
 	});
 });
+// GET Route to Access All Thoughts from a User
+router.get("/users/:username", (req, res) => {
+	console.log(`Querying for thought(s) from ${req.params.username}.`);
+	const params = {
+		TableName: table,
+		KeyConditionExpression: "#un = :user", // Specifies search criteria
+		ExpressionAttributeNames: {
+			"#un": "username", // the # prefix establishes that this is an attribute name alias
+			"#ca": "createdAt",
+			"#th": "thought",
+		},
+		ExpressionAttributeValues: {
+			":user": req.params.username,
+		},
+		ProjectionExpression: "#th, #ca",
+		ScanIndexForward: false,
+	};
+	// get one user's thoughts
+	dynamodb.query(params, (err, data) => {
+		if (err) {
+			console.error(
+				"Unable to query. Error:",
+				JSON.stringify(err, null, 2)
+			);
+			res.status(500).json(err); // an error occurred
+		} else {
+			console.log("Query succeeded.");
+			res.json(data.Items);
+		}
+	});
+}); // closes the route for router.get(users/:username)
+
+// Create new user
+router.post("/users", (req, res) => {
+	const params = {
+		TableName: table,
+		Item: {
+			username: req.body.username,
+			createdAt: Date.now(),
+			thought: req.body.thought,
+			image: req.body.image,
+		},
+	};
+	// database call
+	dynamodb.put(params, (err, data) => {
+		if (err) {
+			console.error(
+				"Unable to add item. Error JSON:",
+				JSON.stringify(err, null, 2)
+			);
+			res.status(500).json(err);
+		} else {
+			console.log("Added item:", JSON.stringify(data, null, 2));
+			res.json({ Added: JSON.stringify(data, null, 2) });
+		}
+	});
+});
 
 module.exports = router;
